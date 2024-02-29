302255bb1d7e9e8a926548d41ade6ec501086521
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2024-02-29 14:49:23 +0000

    used proper naming convention for accordion file

---
+++ accordion/src/components/Accordion.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import styles from ".";
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      <accordion />
+    </div>
+  );
+}
+
+export default Accordion;
