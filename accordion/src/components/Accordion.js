5e73b1c2cf66cc3d4f38a86a46405d1599105827
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2024-03-02 22:04:57 +0000

    accordion.css

--- accordion/src/components/Accordion.js
+++ accordion/src/components/Accordion.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import styles from ".";
+import styles from "../styles/Accordion.css";
+import data from "../data";
 
 function Accordion() {
+  // setting up useState
+  const [selected, setSelected] = useState(null);
   return (
-    <div className="accordion">
-      <accordion />
+    <div className="wrapper">
+      <div className="accordion">{}</div>
     </div>
   );
 }
